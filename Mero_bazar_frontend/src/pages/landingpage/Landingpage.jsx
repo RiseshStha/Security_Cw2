9eafb6dd296a1bbf75e709723e44b72e4b6f82b4
RiseshStha <riseshstha222@gmail.com>
2025-01-17 20:42:09 +0545

    Removing unnecessary code from landing page

--- Mero_bazar_frontend/src/pages/landingpage/Landingpage.jsx
+++ Mero_bazar_frontend/src/pages/landingpage/Landingpage.jsx
@@ -10,32 +10,7 @@ const Landingpage = () => {
     }
   return (
     <>
-    {/* <nav class="navbar navbar-expand-lg bg-white sticky-top bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src={logo} className="rounded-circle" alt="" />
-      <span className='ms-2'>BargainBazzar</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#top">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="#about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="#contact">Contact</a>
-        </li>
-      </ul>
-      <button onClick={handleLogin} className="btn btn-brand ms-lg-3 me-1 rounded-4" type="submit">Login</button>
-      <button onClick={handleRegister} className="btn btn-brand ms-lg-3 rounded-4" type="submit">SignUp</button>
-    </div>
-    </div>
-    </nav> */}
+
     {/* Home */}
     <section id='top' className='min-vh-100 d-flex align-items-center text-center'>
       <div className="container">
